a3fc91fe42d52e1e57d11d9afbd0beb7c11a243a
Bekzhan Kerimkulov <kerimbeka@gmail.com>
2024-02-19 20:56:31 +0000

    created models for fhv

---
+++ homeworks/04-analytics-engineering/models/staging/stg_fhv_tripdata.sql
@@ -0,0 +1,33 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+with tripdata as 
+(
+    
+  select *
+  from {{ source('staging','fhv_tripdata') }}
+
+)
+select
+-- identifiers   
+{{ dbt.safe_cast("dispatching_base_num", api.Column.translate_type("string")) }} as dispatching_base_num,
+{{ dbt.safe_cast("affiliated_base_number", api.Column.translate_type("string")) }} as affiliated_base_number,
+{{ dbt.safe_cast("pulocationid", api.Column.translate_type("integer")) }} as pickup_locationid,
+{{ dbt.safe_cast("dolocationid", api.Column.translate_type("integer")) }} as dropoff_locationid,
+{{ dbt.safe_cast("sr_flag", api.Column.translate_type("integer")) }} as shared_trips_flag,
+
+-- timestamps
+cast(pickup_datetime as timestamp) as pickup_datetime,
+cast(dropoff_datetime as timestamp) as dropoff_datetime
+
+from tripdata
+
+-- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
